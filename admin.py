70aee6c918fed19e80614f2966bf0f7a1c8f7e71
Rupa-Kumari-Mahato <rupadhn145@gmail.com>
2025-05-06 04:51:24 +0530

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,16 +1,22 @@
-from flask import Blueprint, render_template_string,session, redirect
+from flask import Blueprint, render_template, session, redirect
 import sqlite3
 
-admin_bp = Blueprint('admin' ,__name__)
+admin_bp = Blueprint('admin', __name__)
+
 @admin_bp.route('/admin')
-def admin_dashboard();
- if 'role' not in session or session or session['role']!= 'admin':
-  return redirect('/')
- 
- conn = sqlitre3.connect('data/learning_system.db')
- cur = conn.cursor()
- cur.execute(#select user,user name,student marks)
- )
- report = curr.fatchall()
- conn.close()
- return render_templates('admin_panel.html',report=report)
+def admin_dashboard():
+    if 'role' not in session or session['role'] != 'admin':
+        return redirect('/')
+
+    conn = sqlite3.connect('data/learning_system.db')
+    cur = conn.cursor()
+
+    cur.execute('''
+        SELECT users.username, scores.topic, scores.score
+        FROM scores
+        JOIN users ON scores.user_id = users.id
+    ''')
+    report = cur.fetchall()
+    conn.close()
+
+    return render_template('admin_panel.html', report=report)
